7f9162fda08d01ecc3fe9f07a8c707d95dedf8b2
Andrea Franz <andrea@gravityblast.com>
2009-05-07 22:27:40 +0200

    added links to twitter scren_names

--- app.rb
+++ app.rb
@@ -2,6 +2,15 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), 'lib')
 require 'bar_manitto'
 require 'sinatra'
 
+helpers do
+  def tweet_links(text)
+    text.gsub(/@\w+/) do |match|
+      screen_name = match[1, match.size - 1]
+      %|<a href="http://twitter.com/#{screen_name}">@#{screen_name}</a>|
+    end
+  end
+end
+
 get '/' do
   @offset = @params[:offset].to_i > 0 ? params[:offset].to_i : 0
   @tweets = BarManitto::Tweet.all(:order => [:created_at.desc], :limit => 10, :offset => @offset)
